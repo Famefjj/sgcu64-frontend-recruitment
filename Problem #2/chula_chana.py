f612a8a5bc4f9cabfdc9e7b947dd1c301e836c5c
Famefjj <fjj20092@gmail.com>
2021-06-18 23:22:32 +0700

    Update main menu

--- Problem #2/chula_chana.py
+++ Problem #2/chula_chana.py
@@ -1,8 +1,24 @@
 
+
+def main_input_check():
+    main_input = input("Please input any number: ")
+    if main_input == "1":
+        print("valid")
+    elif main_input == "2":
+        pass
+    elif main_input == "3":
+        pass
+    else :
+        print("input incorrect, please try again.")
+        main_input_check()
+
+
 def main():
+    # create main menu
     print("Available commands:")
     print("  1. Check in user")
     print("  2. Check out user")
     print("  3. Print people count")
-    main_input = input("Please input any number: ")
+    main_input_check()
+    
 main()
