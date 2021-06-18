5aba34da080a656cab4e2aafa912254d091399e5
Famefjj <fjj20092@gmail.com>
2021-06-18 14:41:20 +0700

    Add input_check

--- Problem #1/seven_segment.py
+++ Problem #1/seven_segment.py
@@ -1,3 +1,42 @@
+import re
+
+# function for checking input format
+def input_check(input_time):
+
+    # check length of input
+    if len(input_time) != 8:
+        return False
+
+    # check pattern format
+    right_pattern = re.compile(r'\d\d:\d\d:\d\d')
+
+    if right_pattern.search(input_time):
+
+        # if pattern format right, check number of min and sec
+        hr,mi,sec = [int(e) for e in input_time.split(":")]
+        if mi < 60 and sec < 60:
+            return True
+    
+    # return False if not satisfied 2 condition above
+    return False
+
+
+# main function
+def main():
+    input_time = input().strip()
+    
+    # check input format
+    if input_check(input_time):
+        print("okay")
+    else:
+        print("error")
+        
+
+
+# initialize main function      
+main()
+
+
 
     
 
