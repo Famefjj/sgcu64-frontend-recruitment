6c06c1ea22b52b90e0597d7c2b62ca4e72e9d35d
Famefjj <fjj20092@gmail.com>
2021-06-18 23:01:34 +0700

    Add main menu

---
+++ Problem #2/chula_chana.py
@@ -0,0 +1,8 @@
+
+def main():
+    print("Available commands:")
+    print("  1. Check in user")
+    print("  2. Check out user")
+    print("  3. Print people count")
+    main_input = input("Please input any number: ")
+main()
