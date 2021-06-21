e75898ed3996d8e1bb2efffcd9c690c9a80a9fea
Famefjj <fjj20092@gmail.com>
2021-06-21 15:08:44 +0700

    Add validate data

--- Problem #3/script/index.js
+++ Problem #3/script/index.js
@@ -5,11 +5,30 @@ form.addEventListener("submit", event => {
   const data = {}
   for (const [key, value] of formData.entries()) {
     /* USER CODE Begin: Validate data */
-    data[key] = value
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if ((value === "") || (value == null)){
+      alert('Please provide your '+ key +" !")
+      return false
+    }
+    else if ((key === "email") && !re.test(String(value).toLowerCase())){
+      alert("Your email is incorrect !")
+      return false
+    }
+    else if ((key === "confirmpassword") && (value !== data["password"])){
+      alert("Your confirm password not equal to password !")
+      return false
+    }
+    else if ((key === "password") && (value.length < 6)){
+      alert('Your password must contain at least 5 characters')
+      return false
+    }
+    else {
+      data[key] = value
+    }
     /* USER CODE Begin: Validate data */
   }
   console.log(data)
   /* USER CODE Begin: What happened next after recieve form data (Optional) */
-
+  
   /* USER CODE END: What happened next after recieve form data (Optional) */
 })
